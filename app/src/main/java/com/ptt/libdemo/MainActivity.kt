4f490f078ae362da93f50324f1da4989b3c93dca
tungpham <phamthanhtung1995@gmail.com>
2019-05-04 15:36:57 +0700

    update git ignore

--- app/src/main/java/com/ptt/libdemo/MainActivity.kt
+++ app/src/main/java/com/ptt/libdemo/MainActivity.kt
@@ -22,6 +22,6 @@ class MainActivity : AppCompatActivity() {
             )
         ).build()
         mediaComponent.inject(this)
-        imageRepository.queryAllImages()
+        imageRepository.queryAlbum()
     }
 }
