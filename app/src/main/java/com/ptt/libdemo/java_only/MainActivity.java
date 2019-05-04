6b7d6d73f6ce8b6ac54bf336bb90e713ab6961f4
tungpham <phamthanhtung1995@gmail.com>
2019-05-04 22:43:14 +0700

    test lib in Java

---
+++ app/src/main/java/com/ptt/libdemo/java_only/MainActivity.java
@@ -0,0 +1,20 @@
+package com.ptt.libdemo.java_only;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import com.ptt.libdemo.R;
+import com.ptt.mediaquery.MediaQuery;
+
+import java.util.Objects;
+
+@SuppressLint("Registered")
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Objects.requireNonNull(MediaQuery.get().getImageRepository()).queryAlbum();
+    }
+}
--- app/src/main/java/com/ptt/libdemo/kotlin_only/MainActivity.kt
+++ app/src/main/java/com/ptt/libdemo/kotlin_only/MainActivity.kt
@@ -1,15 +1,18 @@
-package com.ptt.libdemo
+package com.ptt.libdemo.kotlin_only
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.ptt.libdemo.R
 import com.ptt.mediaquery.MediaQuery
 
+@SuppressLint("Registered")
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        MediaQuery.instance().imageRepository.queryAlbum()
+        MediaQuery.get().imageRepository.queryAlbum()
     }
 }
