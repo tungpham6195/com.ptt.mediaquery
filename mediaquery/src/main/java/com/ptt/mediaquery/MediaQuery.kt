e494982b19a4ed4c4e68400e00d78bce1551943c
tungpham <phamthanhtung1995@gmail.com>
2019-05-04 22:41:43 +0700

    add comment MediaQuery.kt

--- mediaquery/src/main/java/com/ptt/mediaquery/MediaQuery.kt
+++ mediaquery/src/main/java/com/ptt/mediaquery/MediaQuery.kt
@@ -1,5 +1,6 @@
 package com.ptt.mediaquery
 
+import android.app.Application
 import android.content.Context
 import com.ptt.mediaquery.base.DaggerMediaQueryComponent
 import com.ptt.mediaquery.base.modules.image.ImageModule
@@ -7,6 +8,9 @@ import com.ptt.mediaquery.repositories.image.ImageRepository
 import java.lang.ref.WeakReference
 import javax.inject.Inject
 
+/**
+ * đây là singleton class
+ * */
 class MediaQuery private constructor(context: WeakReference<Context>) {
 
     @Inject
@@ -25,14 +29,23 @@ class MediaQuery private constructor(context: WeakReference<Context>) {
     companion object {
         private lateinit var context: WeakReference<Context>
 
+        /**
+         * todo phải init đầu tiên ở class [Application] của mỗi app
+         * @param context application context của app
+         * @return [MediaQuery]
+         * */
         @JvmStatic
         fun init(context: Context): MediaQuery {
             this.context = WeakReference(context)
             return Holder.INSTANCE
         }
 
+        /**
+         * dùng để lấy get của [MediaQuery] khi muốn sử dụng các repository
+         * @return [MediaQuery]
+         * */
         @JvmStatic
-        fun instance(): MediaQuery {
+        fun get(): MediaQuery {
             return Holder.INSTANCE
         }
     }
