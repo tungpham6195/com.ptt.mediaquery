15a06f6b43fa3fcfb68c569edc580720f84bc5f7
tungpham <phamthanhtung1995@gmail.com>
2019-05-04 22:48:45 +0700

    add comment ImageRepository.kt

--- mediaquery/src/main/java/com/ptt/mediaquery/repositories/image/ImageRepository.kt
+++ mediaquery/src/main/java/com/ptt/mediaquery/repositories/image/ImageRepository.kt
@@ -4,12 +4,18 @@ import com.ptt.mediaquery.model.album.Album
 import com.ptt.mediaquery.model.image.Image
 
 interface ImageRepository {
-    /**todo lấy tất cả hình ảnh*/
+    /**todo lấy tất cả hình ảnh
+     * @return [MutableList] của [Image]
+     * */
     fun queryAllImages(): MutableList<Image>
 
-    /**todo lấy album và cả hình ảnh*/
+    /**todo lấy album và cả hình ảnh
+     * @return [MutableList] của [Album]
+     * */
     fun queryAlbum(): MutableList<Album<Image>>
 
-    /**todo lấy hình theo id của image*/
+    /**todo lấy hình theo id của image
+     * @return [Image]
+     * @param imageId id của hình muốn tìm*/
     fun queryImageById(imageId: Int): Image?
 }
